dcd4d4f7397cf4b1edbc45e2f1f7aed91dd49835
Muhammad Umer <m.umer66288@gmail.com>
2024-10-03 19:11:11 +0500

    Cart Page Done

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,7 +1,6 @@
 <script setup>
 import Navbar from './Navbar.vue';
 import Footer from './Footer.vue';
-import Game from './Game.vue';
 import {
     ShoppingCartIcon,
     ChevronRightIcon,
@@ -17,13 +16,13 @@ import {
 <Navbar />
 <div class="bg-stone-200">
     <div class="max-w-7xl mx-auto sm:px-4 pt-12 pb-0 sm:pb-10">
-        <div class="grid grid-cols-3 md:grid-cols-6 gap-8">
-            <div class="col-span-3 md:col-span-4 px-4 sm:px-0">
-                <h1 class="text-xl md:text-2xl font-semibold text-gray-600 mb-3 md:mb-4">
-                    Cart
-                </h1>
+            <h1 class="text-3xl font-bold text-gray-800 text-center">Shopping Cart</h1>
+
+            <div class="grid md:grid-cols-3 gap-8 mt-16">
+                <div class="md:col-span-2 space-y-4">
+                    <div>
                 <!-- Empty Cart -->
-                <!-- <div class="p-5 sm:p-8 bg-stone-300 rounded-xl">
+                <!-- <div class="p-5 sm:p-8 bg-gray-100 rounded-xl">
                     <ShoppingCartIcon class="size-12 md:size-16 text-[#0c1b51] mx-auto" />
                     <h1 class="text-xl md:text-2xl font-semibold text-gray-600 my-3 md:my-5 text-center">
                         Your cart is empty
@@ -36,73 +35,62 @@ import {
                     </div>
                 </div> -->
                 <!-- Empty Cart -->
-                <div class="p-3 md:p-5 bg-stone-300 rounded-xl mb-10 md:mb-14">
-                    <div class="flex justify-between">
-                        <div class="flex items-center gap-2 md:gap-3">
-                            <RouterLink to="/gameDetails" class="h-full">
-                                <img src="https://gaming-cdn.com/images/products/13615/250x143/13615-cover.jpg?v=1727777270" alt="" class="rounded-lg h-full w-32 object-cover">
-                            </RouterLink>
-                            <div class="flex flex-col gap-6 justify-between h-full">
-                                <div class="">
-                                    <h3 class="text-gray-600 font-bold sm:text-base text-sm md:text-lg">Ravenswatch</h3>
-                                    <p class="text-sm text-gray-600">steam</p>
-                                </div>
+                    <div v-for="(item, index) in 3" :key="index" class="grid grid-cols-3 items-start gap-6 py-4 border-b border-gray-300">
+                        <div class="col-span-2 flex items-start gap-4">
+                            <div class="w-28 h-28 max-sm:w-24 max-sm:h-24 shrink-0 bg-gray-100 p-2 rounded-md">
+                                <img src='https://readymadeui.com/images/product14.webp' class="w-full h-full object-contain" />
+                            </div>
 
-                            </div>
-                        </div>
-                        <div class="flex flex-col justify-center gap-1 md:gap-2">
-                            <span class="text-gray-600 font-semibold text-end text-sm sm:text-base md:text-lg">14.72€</span>
-                            <div class="flex">
-                                <button class="text-red-500 hover:text-red-600 mr-2">
-                                    <TrashIcon class="size-5" />
-                                </button>
-                                <button class="border border-slate-400 flex justify-center items-center py-1 size-7 text-gray-800">
-                                    <PlusIcon class="size-4"/>
-                                </button>
-                                <span class="border border-slate-400 flex justify-center items-center py-1 size-7 text-gray-800">1</span>
-                                <button class="border border-slate-400 flex justify-center items-center py-1 size-7 text-gray-800">
-                                    <MinusIcon class="size-4"/>
+                            <div class="flex flex-col">
+                                <h3 class="text-base font-bold text-gray-800">Velvet Sneaker</h3>
+                                <p class="text-xs font-semibold text-gray-500 mt-0.5">Size: MD</p>
+
+                                <button type="button" class="mt-6 font-semibold text-red-500 text-xs flex items-center gap-1 shrink-0">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-4 fill-current inline" viewBox="0 0 24 24">
+                                        <path d="M19 7a1 1 0 0 0-1 1v11.191A1.92 1.92 0 0 1 15.99 21H8.01A1.92 1.92 0 0 1 6 19.191V8a1 1 0 0 0-2 0v11.191A3.918 3.918 0 0 0 8.01 23h7.98A3.918 3.918 0 0 0 20 19.191V8a1 1 0 0 0-1-1Zm1-3h-4V2a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v2H4a1 1 0 0 0 0 2h16a1 1 0 0 0 0-2ZM10 4V3h4v1Z" data-original="#000000"></path>
+                                        <path d="M11 17v-7a1 1 0 0 0-2 0v7a1 1 0 0 0 2 0Zm4 0v-7a1 1 0 0 0-2 0v7a1 1 0 0 0 2 0Z" data-original="#000000"></path>
+                                    </svg>
+                                    REMOVE
                                 </button>
                             </div>
                         </div>
+
+                        <div class="ml-auto">
+                            <h4 class="text-lg max-sm:text-base font-bold text-gray-800">$20.00</h4>
+
+                            <button type="button"
+                                class="mt-6 flex items-center px-3 py-1.5 border border-gray-300 text-gray-800 text-xs outline-none bg-transparent rounded-md">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-2.5 fill-current" viewBox="0 0 124 124">
+                                    <path d="M112 50H12C5.4 50 0 55.4 0 62s5.4 12 12 12h100c6.6 0 12-5.4 12-12s-5.4-12-12-12z" data-original="#000000"></path>
+                                </svg>
+
+                                <span class="mx-3 font-bold">2</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-2.5 fill-current" viewBox="0 0 42 42">
+                                    <path d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z" data-original="#000000"></path>
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+
+                </div>
+                </div>
+
+                <div class="bg-gray-100 rounded-md p-4 h-max sticky sm:relative bottom-0 inset-x-0 w-full">
+                    <h3 class="text-lg max-sm:text-base font-bold text-gray-800 border-b border-gray-300 pb-2">Order Summary</h3>
+
+                    <ul class="text-gray-800 mt-6 space-y-3">
+                        <li class="flex flex-wrap gap-4 text-sm">Total <span class="ml-auto font-bold">$200.00</span></li>
+                        <li class="flex flex-wrap gap-4 text-sm">Shipping <span class="ml-auto font-bold">Free</span></li>
+                        <hr class="border-gray-300" />
+                        <li class="flex flex-wrap gap-4 text-sm font-bold">SubTotal <span class="ml-auto">$200.00</span></li>
+                    </ul>
+
+                    <div class="mt-6 space-y-3">
+                        <button type="button" class="text-sm px-4 py-2.5 w-full font-semibold tracking-wide bg-gray-800 hover:bg-gray-900 text-white rounded-md">Checkout</button>
+                        <button type="button" class="text-sm px-4 py-2.5 w-full font-semibold tracking-wide bg-transparent text-gray-800 border border-gray-300 rounded-md">Continue Shopping  </button>
                     </div>
                 </div>
-                <div>
-                    <h1 class="text-xl md:text-2xl font-semibold text-gray-600 mb-3 md:mb-4">
-                        Recommended
-                    </h1>
-            <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
-                <Game v-for="(item, index) in 9" :key="index" />
             </div>
-                </div>
-            </div>
-            <div class="col-span-3 md:col-span-2 sticky sm:relative bottom-0 inset-x-0 w-full">
-                <h1 class="text-xl md:text-2xl font-semibold text-gray-600 mb-3 md:mb-4 hidden sm:block">
-                    Summary
-                </h1>
-                <div class="p-4 lg:p-6 bg-stone-50 shadow-md sm:rounded-xl">
-                    <div class="flex justify-between mb-1 sm:mb-3 text-gray-600 lg:text-base md:text-sm text-sm">
-                        <span>Total</span>
-                        <span>0€</span>
-                    </div>
-                    <div class="flex justify-between mb-1 sm:mb-3 text-gray-600 lg:text-base md:text-sm text-sm">
-                        <span>Shipping</span>
-                        <span>Free</span>
-                    </div>
-                    <div class="flex justify-between mb-1 sm:mb-3 text-gray-600 lg:text-base md:text-sm text-base">
-                        <span class="font-semibold">Subtotal</span>
-                        <span class="font-semibold">56€</span>
-                    </div>
-                    <button type="button" class="bg-[#535C91] hover:bg-[#384176] w-full  text-white lg:text-base md:text-sm text-base rounded-full p-2 transition-all my-3 sm:my-6">
-                        <span>Go to payment</span>
-                    </button>
-                    <RouterLink to="/shope" class="hidden sm:flex justify-center items-center gap-1 lg:text-base md:text-sm sm:text-base hover:text-gray-900 text-gray-600 pt-3 border-t border-[#535C91]">
-                        <ChevronLeftIcon class="size-4 mt-1" />
-                        <span>Continue shopping</span>
-                    </RouterLink>
-                </div>
-            </div>
-        </div>
     </div>
 </div>
 <Footer />
