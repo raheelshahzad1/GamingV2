92b93afa38a8a7f64ce4b25167fa700fea5d930a
Muhammad Raheel <raheelshahzad114422@gmail.com>
2024-10-04 16:16:27 +0500

    cart fixes

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -16,11 +16,11 @@ import {
   <Navbar />
   <div class="bg-stone-200">
     <div class="max-w-7xl mx-auto sm:px-4 pt-12 pb-0 sm:pb-10">
-      <h1 class="text-3xl font-bold text-gray-800 text-center">
+      <h1 class="text-3xl font-bold text-gray-800 text-center mb-8 sm:mb-16">
         Shopping Cart
       </h1>
 
-      <div class="grid md:grid-cols-3 gap-8 mt-16">
+      <div class="grid md:grid-cols-3 gap-14">
         <div class="md:col-span-2 space-y-4">
           <div class="px-4 sm:px-0">
             <!-- <div class="p-5 sm:p-8 bg-gray-100 rounded-xl">
@@ -36,58 +36,47 @@ import {
                     </div>
                 </div> -->
 
-            <div
-              v-for="(item, index) in 3"
-              :key="index"
-              class="grid grid-cols-5 gap-3 py-4 border-b border-gray-300"
-            >
-              <div class="col-span-1 bg-gray-100 p-2 rounded-md">
-                <RouterLink to="/gameDetails">
-                  <img
-                    src="https://readymadeui.com/images/product14.webp"
-                    class="w-full h-full object-contain"
-                  />
-                </RouterLink>
+            <div v-for="(item, index) in 3" :key="index" class="flex gap-10 py-4 border-b border-gray-300">
+              <div class="shrink-0 h-28 w-28 bg-gray-50 p-3 rounded-md">
+                <img src="https://readymadeui.com/images/product14.webp" class="w-full h-full object-contain" />
               </div>
-              <div
-                class="col-span-4 flex justify-between flex-col md:flex-row gap-2 md:gap-4"
-              >
-                <div>
-                  <h3
-                    class="text-sm sm:text-base font-semibold text-gray-800 line-clamp-3"
-                  >
+
+              <div class="w-full flex flex-col sm:flex-row gap-6 sm:gap-10">
+                <div class="grow">
+                  <h3 class="text-sm sm:text-base font-semibold text-gray-800 line-clamp-3">
                     Velvet Sneaker Velvet Sneaker Velvet Sneaker Velvet Sneaker
                   </h3>
+
                   <button
-                    class="md:flex hidden text-sm items-center mt-4 md:mt-6 gap-1 font-medium text-red-500 hover:text-red-600"
-                  >
+                    class="md:flex hidden text-sm items-center mt-4 md:mt-6 gap-1 font-medium text-red-500 hover:text-red-600">
                     <TrashIcon class="size-4" />
                     <span>REMOVE</span>
                   </button>
                 </div>
-                <div>
-                  <h4
-                    class="text-lg max-sm:text-base font-bold text-gray-800 md:text-end mb-2"
-                  >
+
+                <div class="flex sm:flex-col gap-8 items-end">
+                  <h4 class="text-lg max-sm:text-base font-bold text-gray-800 mb-2">
                     $20.00
                   </h4>
+
                   <div class="flex items-center gap-2">
-                    <div class="flex border border-gray-400">
-                      <button class="py-1 px-2">
+                    <button class="md:hidden">
+                      <TrashIcon class="size-4 text-red-500 hover:text-red-600" />
+                    </button>
+
+                    <div class="flex rounded-md border border-gray-400">
+                      <button class="h-8 w-8 flex items-center justify-center">
                         <PlusIcon class="size-3" />
                       </button>
-                      <span class="py-1 px-2 border-l border-r border-gray-400"
-                        >1</span
-                      >
-                      <button class="py-1 px-2">
+
+                      <span class="h-8 w-8 flex items-center justify-center border-l border-r border-gray-400">
+                        1
+                      </span>
+
+                      <button class="h-8 w-8 flex items-center justify-center">
                         <MinusIcon class="size-3" />
                       </button>
                     </div>
-                    <button class="md:hidden">
-                      <TrashIcon
-                        class="size-5 text-red-500 hover:text-red-600"
-                      />
-                    </button>
                   </div>
                 </div>
               </div>
@@ -96,11 +85,8 @@ import {
         </div>
 
         <div
-          class="shadow-[0_-5px_5px_-5px_grey] sm:shadow-none border-t bg-gray-100 sm:rounded-md p-2 sm:p-4 h-max sticky sm:relative bottom-0 inset-x-0 w-full"
-        >
-          <h3
-            class="hidden sm:block text-lg max-sm:text-base font-bold text-gray-800 border-b border-gray-300 pb-2"
-          >
+          class="shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] sm:shadow-none border-t bg-gray-100 sm:rounded-md p-4 h-max sticky sm:relative bottom-0 inset-x-0 w-full">
+          <h3 class="hidden sm:block text-lg max-sm:text-base font-bold text-gray-800 border-b border-gray-300 pb-2">
             Order Summary
           </h3>
 
@@ -118,16 +104,12 @@ import {
           </ul>
 
           <div class="mt-2 sm:mt-6 space-y-3">
-            <button
-              type="button"
-              class="text-sm px-4 py-2.5 w-full font-semibold tracking-wide bg-gray-800 hover:bg-gray-900 text-white rounded-md"
-            >
+            <button type="button"
+              class="text-sm px-4 py-2.5 w-full font-semibold tracking-wide bg-gray-800 hover:bg-gray-900 text-white rounded-md">
               Checkout
             </button>
-            <button
-              type="button"
-              class="hidden sm:inline text-sm px-4 py-2.5 w-full font-semibold tracking-wide bg-transparent text-gray-800 border border-gray-300 rounded-md"
-            >
+            <button type="button"
+              class="hidden sm:inline text-sm px-4 py-2.5 w-full font-semibold tracking-wide bg-transparent text-gray-800 border border-gray-300 rounded-md">
               Continue Shopping
             </button>
           </div>
